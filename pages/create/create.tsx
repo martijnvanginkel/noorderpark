ff570eb70b0fdba733c348abbab059ce9b562e4f
Martijn van Ginkel <martijnvanginkel2@gmail.com>
2022-03-24 13:34:57 +0100

    first commit

---
+++ pages/create/create.tsx
@@ -0,0 +1,16 @@
+
+import Link from 'next/link'
+
+function Create() {
+  return (
+      <div>
+          hoi
+
+        <Link href="/">
+            <a>back!</a>
+        </Link>
+      </div>
+  )
+}
+
+export default Create
